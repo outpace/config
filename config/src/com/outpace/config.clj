8a0ac5c13dc9d8605ca46e994f6f79a5e12b7a54
Alexander Taggart <alex.taggart@outpace.com>
2014-05-07 13:06:47 -0400

    Fix arity transfer bug with macros.

--- config/src/com/outpace/config.clj
+++ config/src/com/outpace/config.clj
@@ -38,14 +38,14 @@
          (def ~name (get config qname#))
          (def ~name))))
   ([name default-val]
-    (defconfig name nil default-val))
+    `(defconfig ~name nil ~default-val))
   ([name doc default-val]
     `(let [qname# (symbol (str *ns*) (str '~name))
            default-val# ~default-val]
        (swap! defaults assoc qname# default-val#)
        (swap! required disj qname#)
        (let [val# (get config qname# default-val#)]
-         (if ~doc
-           (def ~name ~doc val#)
-           (def ~name val#))))))
+         ~(if doc
+            `(def ~name ~doc val#)
+            `(def ~name val#))))))
 
