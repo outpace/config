cb05e0f02744f161beae13ff4bb20a02b8d12f1e
Alexander Taggart <alex.taggart@outpace.com>
2014-05-07 12:21:57 -0400

    Add missing docstrings.

--- config/src/com/outpace/config.clj
+++ config/src/com/outpace/config.clj
@@ -2,7 +2,10 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]))
 
-(defn valid-key? [k]
+(defn valid-key?
+  "Returns true IFF k is acceptable as a key in config.edn,
+   i.e., a namespaced symbol."
+  [k]
   (and (symbol? k) (namespace k)))
 
 (if-let [source (or (System/getProperty "config.edn")
@@ -24,6 +27,9 @@
   required (atom #{}))
 
 (defmacro defconfig
+  "Same as (def name doc-string? init?) except the var's value may be configured
+   at load-time by this library. Note that default-val will be evaluated, even
+   if there is a configured value"
   ([name]
     `(let [qname# (symbol (str *ns*) (str '~name))]
        (swap! required conj qname#)
