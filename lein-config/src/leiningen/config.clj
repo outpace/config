e3b17f721ab17ac998fe0c3666bd9c14c7a3219d
Alexander Taggart <alex.taggart@outpace.com>
2014-05-08 15:17:39 -0400

    Improve generated config.edn.

--- lein-config/src/leiningen/config.clj
+++ lein-config/src/leiningen/config.clj
@@ -4,6 +4,68 @@
             [leiningen.core.eval :as eval]
             [leiningen.core.main :as main]))
 
+(def doc-str
+  `(fn [sym#]
+     (if-let [doc# (try (-> sym# find-var meta :doc) (catch Exception e# nil))]
+       (let [[line# & more#] (->> (str/split-lines doc#)
+                               (drop-while str/blank?)
+                               (reverse)
+                               (drop-while str/blank?)
+                               (reverse))
+             trim-len# (->> more#
+                         (map #(re-find #"^\s*" %))
+                         (map count)
+                         (apply min 0))
+             doc-lines# (cons line# (map #(.substring % trim-len#) more#))]
+         (with-out-str
+           (doseq [doc-line# doc-lines#]
+             (println ";" doc-line#))))
+       "")))
+
+(def unbound-str
+  `(fn [sym#]
+     (str (~doc-str sym#) "#_" (prn-str sym#))))
+
+(def entry-str
+  `(fn [sym# val# def#]
+     (let [sym-str# (pr-str sym#)
+           val-str# (pr-str val#)
+           def-str# (when def# (pr-str def#))
+           split?#  (or (.contains val-str# "\n")
+                        (and def-str# (.contains def-str# "\n"))
+                        (< 80 (if def-str#
+                                (+ (count sym-str#) 1 (count val-str#) 3 (count def-str#))
+                                (+ (count sym-str#) 1 (count val-str#)))))]
+       (with-out-str
+         (print (~doc-str sym#))
+         (if split?#
+           (do
+             (println sym-str#)
+             (println val-str#)
+             (when def-str#
+               (print "#_") (println def-str#)))
+           (do
+             (print sym-str#) (print " ") (print val-str#)
+             (if def-str#
+               (do (print " #_") (println def-str#))
+               (println))))))))
+
+(def default-str
+  `(fn [sym# def#]
+     (let [sym-str# (pr-str sym#)
+           def-str# (pr-str def#)
+           split?#  (or (.contains def-str# "\n")
+                        (< 80 (+ 2 (count sym-str#)
+                                 3 (count def-str#))))]
+       (with-out-str
+         (print (~doc-str sym#))
+         (if split?#
+           (do
+             (print "#_") (println sym-str#)
+             (print "#_") (println def-str#))
+           (do
+             (print "#_") (print sym-str#) (print " #_") (println def-str#)))))))
+
 (def generate-file
   `(let [config-map#     conf/config
          default-map#    @conf/defaults
@@ -12,56 +74,48 @@
          default-keyset# (set (keys default-map#))
          available-set#  (set/union config-keyset# default-keyset#)
          wanted-set#     (set/union required-set# default-keyset#)
-         missing-set#    (set/difference required-set# available-set#)
+         unbound-set#    (set/difference required-set# available-set#)
          unused-set#     (set/difference available-set# wanted-set#)
-         explicit-set#   (set/intersection config-keyset# wanted-set#)
-         implicit-set#   (set/difference default-keyset# explicit-set#)
+         used-set#       (set/intersection available-set# wanted-set#)
+         unbound-str#    ~unbound-str
+         entry-str#      ~entry-str
+         default-str#    ~default-str
+         newline#        (println-str)
          content#        (with-out-str
                            (println "{")
-                           (when (seq missing-set#)
+                           (when (seq unbound-set#)
                              (println)
-                             (println ";; REQUIRED CONFIG VARS WITHOUT VALUES -- FIX THEM.")
+                             (println ";; UNBOUND CONFIG VARS:")
                              (println)
-                             (println "#_ #{")
-                             (doseq [sym# missing-set#]
-                               (when-let [doc# (:doc (meta (find-var sym#)))]
-                                 (doseq [doc-line# (str/split-lines doc#)]
-                                   (println ";" doc-line#)))
-                               (prn sym#)
-                               (println))
-                             (println "}")
+                             (print (->> unbound-set#
+                                      (sort)
+                                      (map unbound-str#)
+                                      (interpose newline#)
+                                      (apply str)))
                              (println))
                            (when (seq unused-set#)
                              (println)
-                             (println ";; UNUSED CONFIG ENTRIES.")
+                             (println ";; UNUSED CONFIG ENTRIES:")
                              (println)
-                             (doseq [sym# unused-set#]
-                               (prn sym# (get config-map# sym#))
-                               (println))
+                             (print (->> unused-set#
+                                      (sort)
+                                      (map (fn [sym#]
+                                             (entry-str# sym# (get config-map# sym#) nil)))
+                                      (interpose newline#)
+                                      (apply str)))
                              (println))
-                           (when (seq explicit-set#)
+                           (when (seq used-set#)
                              (println)
-                             (println ";; CONFIG VARS.")
+                             (println ";; CONFIG ENTRIES:")
                              (println)
-                             (doseq [sym# explicit-set#]
-                               (when-let [doc# (:doc (meta (find-var sym#)))]
-                                 (doseq [doc-line# (str/split-lines doc#)]
-                                   (println ";" doc-line#)))
-                               (prn sym# (get config-map# sym#))
-                               (println))
-                             (println))
-                           (when (seq implicit-set#)
-                             (println)
-                             (println ";; DEFAULTED CONFIG VARS.")
-                             (println)
-                             (println "#_ {")
-                             (doseq [sym# implicit-set#]
-                               (when-let [doc# (:doc (meta (find-var sym#)))]
-                                 (doseq [doc-line# (str/split-lines doc#)]
-                                   (println ";" doc-line#)))
-                               (prn sym# (get default-map# sym#))
-                               (println))
-                             (println "}")
+                             (print (->> used-set#
+                                      (sort)
+                                      (map (fn [sym#]
+                                             (if (contains? config-keyset# sym#)
+                                               (entry-str# sym# (get config-map# sym#) (get default-map# sym#))
+                                               (default-str# sym# (get default-map# sym#)))))
+                                      (interpose newline#)
+                                      (apply str)))
                              (println))
                            (println "}"))]
      (spit "config.edn" content#)))
