7522dd9bd82f4ecc7c7a54ef0278aeeb5a34a1dc
Alexander Taggart <alex.taggart@outpace.com>
2014-05-08 15:34:33 -0400

    Remove example app; doc is sufficient.

--- example/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject com.outpace/config-example "0.1.0"
-  :dependencies [[com.outpace/config "0.1.0"]
-                 [org.clojure/clojure "1.5.1"]]
-  :plugins [[com.outpace/lein-config "0.1.0"]]
-  :main com.outpace.config-example)
