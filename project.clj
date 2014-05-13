4ef0613f3af97d09c4dc97e468288677d474192c
Alexander Taggart <alex.taggart@outpace.com>
2014-05-13 17:06:36 -0400

    Prepare for opening.

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
 (defproject com.outpace/config "0.1.0"
   :description "A library for declaring and setting configuration vars."
   :url "https://github.com/outpace/config"
-  :plugins [[lein-maven-s3-wagon "0.2.3"]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.4"]]
-  :deploy-repositories [["private" {:url "s3://outpace-maven/releases/"
-                                    :username :env/aws_access_key_id
-                                    :passphrase :env/aws_secret_access_key
-                                    :sign-releases false}]])
+                 [org.clojure/tools.namespace "0.2.4"]])
