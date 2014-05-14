06012aeb70d98c1844ade349eec40eee127061ec
Alexander Taggart <alex.taggart@outpace.com>
2014-05-13 21:14:15 -0400

    Update group id.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.outpace/config "0.1.0"
+(defproject outpace/config "0.1.0"
   :description "A library for declaring and setting configuration vars."
   :url "https://github.com/outpace/config"
   :license {:name "Eclipse Public License"
