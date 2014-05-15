ef2c5efc4ca1b95f4b74f7428aa48042baebd963
Alexander Taggart <alex.taggart@outpace.com>
2014-05-15 15:52:01 -0400

    Conform to updated outsourcing policy.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject outpace/config "0.1.0"
+(defproject com.outpace/config "0.1.0"
   :description "A library for declaring and setting configuration vars."
   :url "https://github.com/outpace/config"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.4"]])
