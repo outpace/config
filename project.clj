6f34ec67f61d2cc0047ba7f3c9aedc35c0d67789
Paul Stadig <paul@stadig.name>
2014-11-26 11:08:39 -0500

    Version 0.7.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.outpace/config "0.7.1-SNAPSHOT"
+(defproject com.outpace/config "0.7.1"
   :description "A library for declaring and setting configuration vars."
   :url "https://github.com/outpace/config"
   :license {:name "Apache License, Version 2.0"
