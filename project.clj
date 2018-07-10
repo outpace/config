c4b1e461eb1ce3fa931ee13f4e868c87f7a85d38
Daniel Solano Gómez <daniel.solano@outpace.com>
2018-07-10 17:23:15 -0500

    Update dependencies, make Clojure provided

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/outpace/config"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.5"]]
-  :plugins [[lein-codox "0.10.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/tools.namespace "0.2.10"]]
+  :plugins [[lein-codox "0.10.4"]]
   :codox {:src-dir-uri "http://github.com/outpace/config/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :deploy-repositories [["releases" :clojars]])
