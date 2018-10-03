bb9d28e9888603dfced40c89f9dfaf5927b9d24f
Daniel Solano Gómez <daniel.solano@outpace.com>
2018-10-03 14:42:32 -0500

    Bump release to 0.13.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.outpace/config "0.13.0"
+(defproject com.outpace/config "0.13.1-SNAPSHOT"
   :description "A library for declaring and setting configuration vars."
   :url "https://github.com/outpace/config"
   :license {:name "Apache License, Version 2.0"
