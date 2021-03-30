7daaf7e828e2b2295da184c260a1af0e646b86b9
Daniel Solano Gómez <daniel.solano@outpace.com>
2021-03-30 14:31:08 -0500

    Update dependencies
    Co-authored-by: Rusty Bentley <mycoding.alert@gmail.com>

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/outpace/config"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/tools.namespace "0.2.10"]]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
+                 [org.clojure/tools.namespace "1.1.0"]]
   :profiles {:dev {:resource-paths ["test_resources"]}}
-  :plugins [[lein-codox "0.10.4"]]
+  :plugins [[lein-codox "0.10.7"]]
   :codox {:src-dir-uri "http://github.com/outpace/config/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :deploy-repositories [["releases" :clojars]])
